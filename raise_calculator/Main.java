c8b5cb849be86fb1b328f088f1fe587858c5100e
william winsett <wrwinsett@gmail.com>
2023-05-10 16:23:44 -0400

    refined code to prevent resource link

--- raise_calculator/Main.java
+++ raise_calculator/Main.java
@@ -5,40 +5,39 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class Main {
-	public static void main(String[] args) {
-		String employeesFolder = "employees.txt";
-		File employeeData = new File(employeesFolder);
-		EmployeeRecord employeeRecord = null;
-		
-		if (employeeData.exists()) {
-            String searchKeyword = "Name";
-            int lineNumber = 0;
-            
+    private static final String SEARCH_KEYWORD = "Name";
+
+    public static void main(String[] args) {
+        String employeesFolder = "employees.txt";
+        File employeeData = new File(employeesFolder);
+        EmployeeRecord employeeRecord = null;
+
+        if (employeeData.exists()) {
             try (BufferedReader br = new BufferedReader(new FileReader(employeesFolder))) {
                 String line;
-                
+                int lineNumber = 0;
+
                 while ((line = br.readLine()) != null) {
                     lineNumber++;
-                    if (line.contains(searchKeyword)) {
-                    	String name = line.trim().split(":")[1].trim();
-                        String salary = br.readLine().trim().split(":")[1].trim();
-                        String travel = br.readLine().trim().split(":")[1].trim();
+                    if (line.contains(SEARCH_KEYWORD)) {
+                        String name = extractValue(line);
+                        String salary = extractValue(br.readLine());
+                        String travel = extractValue(br.readLine());
                         employeeRecord = new EmployeeRecord(name, salary, travel);
-                        
+
                         System.out.println(employeeRecord);
-                        }
-                    
+                    }
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                System.err.println("Error reading employee data file: " + e.getMessage());
             }
-            
-            
         } else {
-            System.out.println("your employees folder could not be found");
+            System.err.println("Employee data file not found: " + employeesFolder);
         }
-	}
-	
+    }
+
+    private static String extractValue(String line) {
+        return line.replaceAll(".*:", "").trim();
+    }
 }
